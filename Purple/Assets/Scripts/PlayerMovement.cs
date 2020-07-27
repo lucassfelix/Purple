d9112d2e997f0bdfc6551c6c458d3365f985f3f7
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-27 19:28:14 -0300

    Player Movement Recriated
    Slippery as ever

---
+++ Purple/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _moveSpeed = 6f;
+    private Rigidbody _rigidbody;
+    private Vector3 _moveDirection;
+
+    private bool ShouldMove => _moveDirection != Vector3.zero;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+     void FixedUpdate() 
+    {
+        if(ShouldMove)
+        {
+            _rigidbody.velocity = _moveDirection * _moveSpeed;
+        }    
+    }
+}
