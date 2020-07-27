2aaaec21d02d63caaa782a345ce3e5c22ac4a24f
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-27 20:51:35 -0300

    Field of View functional
    Obstacles get detected

---
+++ Purple/Assets/Scripts/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow._viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle(-fow._viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle(fow._viewAngle / 2, false);
+
+    }
+}
