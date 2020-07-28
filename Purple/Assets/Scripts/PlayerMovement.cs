ababc49e086223f8911ca0a7a637f0a12f0975e6
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-28 11:19:32 -0300

    Player Switch Implemented
    Implemented with the observer pattern

--- Purple/Assets/Scripts/PlayerMovement.cs
+++ Purple/Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,57 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float _moveSpeed = 6f;
-    private Rigidbody _rigidbody;
     private Vector3 _moveDirection;
+
+    private Rigidbody _currentRigidbody;
+    private Rigidbody _redRigidbody;
+    private Rigidbody _blueRigidbody;
 
     private bool ShouldMove => _moveDirection != Vector3.zero;
 
+    void Awake()
+    {
+        _redRigidbody = GameObject.Find("RedPlayer").GetComponent<Rigidbody>();
+        _blueRigidbody = GameObject.Find("BluePlayer").GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        _currentRigidbody = _redRigidbody;
+
+        PlayerSwitch.OnPlayerSwitched += SwitchPlayers;
     }
 
     void Update()
     {
         _moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            SwitchPlayers(true);
+
     }
 
-     void FixedUpdate() 
+
+    void SwitchPlayers(bool _isRed)
+    {
+        if(_isRed)
+        {
+            //Switch to blue
+            _currentRigidbody = _blueRigidbody;
+            //return
+        }
+        else
+        {
+            //Switch to red
+            _currentRigidbody = _redRigidbody;
+        }
+    }
+
+    void FixedUpdate() 
     {
         if(ShouldMove)
         {
-            _rigidbody.velocity = _moveDirection * _moveSpeed;
+            _currentRigidbody.velocity = _moveDirection * _moveSpeed;
         }    
     }
 }
---
+++ Purple/Assets/Scripts/PlayerSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerSwitch : MonoBehaviour
+{
+    public static event Action<bool> OnPlayerSwitched;
+
+    private bool _redIsCurrentPlayer = false;
+
+    private void SwitchPlayers()
+    {
+        if(OnPlayerSwitched != null)
+        {
+            OnPlayerSwitched(_redIsCurrentPlayer);
+        }
+
+        _redIsCurrentPlayer = !_redIsCurrentPlayer;
+    }
+}
