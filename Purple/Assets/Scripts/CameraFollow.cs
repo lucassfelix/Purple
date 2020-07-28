880980cb227da6862014048b2a15b0c441d639b9
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-28 15:34:53 -0300

    Camera Follow Implemented

---
+++ Purple/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    
+    private Rigidbody _playerRigidbody;
+
+    void Awake()
+    {
+        PlayerSwitch.OnPlayerSwitched += CameraSwitch;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(_playerRigidbody.transform.position.x,10,_playerRigidbody.transform.position.z);
+    }
+
+    void CameraSwitch(Rigidbody _newPlayerRb)
+    {
+        _playerRigidbody = _newPlayerRb;
+    }
+
+
+
+}
--- Purple/Assets/Scripts/PlayerSwitch.cs
+++ Purple/Assets/Scripts/PlayerSwitch.cs
@@ -3,15 +3,34 @@ using UnityEngine;
 
 public class PlayerSwitch : MonoBehaviour
 {
-    public static event Action<bool> OnPlayerSwitched;
+    public static event Action<Rigidbody> OnPlayerSwitched;
 
-    private bool _redIsCurrentPlayer = false;
+    private bool _redIsCurrentPlayer = true;
+    private Rigidbody _redPlayer;
+    private Rigidbody _bluePlayer;
+
+    void Awake() {
+        _redPlayer = GameObject.Find("RedPlayer").GetComponent<Rigidbody>();
+        _bluePlayer = GameObject.Find("BluePlayer").GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        OnPlayerSwitched(_redPlayer);
+    }
 
     private void SwitchPlayers()
     {
         if(OnPlayerSwitched != null)
         {
-            OnPlayerSwitched(_redIsCurrentPlayer);
+            if(_redIsCurrentPlayer)
+            {
+                OnPlayerSwitched(_redPlayer);
+            }
+            else
+            {
+                OnPlayerSwitched(_bluePlayer);
+            }
         }
 
         _redIsCurrentPlayer = !_redIsCurrentPlayer;
