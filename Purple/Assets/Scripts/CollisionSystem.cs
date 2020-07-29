6f60c0852ab11068feaf42ee4d950acffd9de17f
Lucas Salaverry Félix <lucas.salaverry@edu.pucrs.br>
2020-07-29 10:31:57 -0300

    -Collision system working  -Touching already painted block does not play particle animation again

---
+++ Purple/Assets/Scripts/CollisionSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CollisionSystem : MonoBehaviour
+{
+    [SerializeField] private string _paintableObjectTag;
+    [SerializeField] private string _paintedObjectTag;
+
+    
+    private Material _playerMaterial;
+    private ParticleSystem _playerParticleSystem;
+
+    private void Start()
+    {
+        _playerMaterial = GetComponent<MeshRenderer>().material;
+        _playerParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(_paintableObjectTag))
+        {
+            _playerParticleSystem.Play();
+            other.gameObject.GetComponent<MeshRenderer>().material = _playerMaterial;
+            other.gameObject.tag = _paintedObjectTag;
+        }
+    }
+}
